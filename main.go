c54a197efaa294828d9cba3055dafbc258245299
imduchuyyy <duchuy.124dk@gmail.com>
2024-12-18 09:38:37 +0700

    feat: init cmd

--- main.go
+++ main.go
@@ -1,31 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-func print(number uint) {
-	// Print the number to the console
-	fmt.Println(number)
-}
+import "github.com/helixpay-xyz/indexer/cmd"
 
 func main() {
-	start := time.Now() // Start timer
-
-	var wg sync.WaitGroup
-
-	for i := 1; i <= 100; i++ {
-		wg.Add(1)
-		func(n uint) {
-			defer wg.Done()
-			print(n)
-		}(uint(i))
-	}
-
-	wg.Wait()
-
-	elapsed := time.Since(start) // Calculate elapsed time
-	fmt.Printf("Execution time: %s\n", elapsed)
+	cmd.Execute()
 }
